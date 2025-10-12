69b11f686b8c1a69659415cf278f2989aa2bc95a
Jullisa Rodriguez <jullisa.rodriguez4@gmail.com>
2025-10-12 16:06:04 -0700

    new project

--- src/main/java/com/yearup/HomeScreen.java
+++ src/main/java/com/yearup/HomeScreen.java
@@ -6,16 +6,38 @@ public class HomeScreen {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
 
-        while(running) {
+//        The home screen should give the user the following options. The
+//        application should continue to run until the user chooses to exit.
+//§ D) Add Deposit - prompt user for the deposit information and
+//        save it to the csv file
+//§ P) Make Payment (Debit) - prompt user for the debit
+//        information and save it to the csv file
+//§ L) Ledger - display the ledger screen
+//§ X) Exit - exit the application
+
+        while (running) {
             System.out.println("Welcome to Malevolent Ledger, where we handle your money like it's our own!");
-            System.out.println("1. add a deposit?");
-            System.out.println("2. Make a Payment");
-            System.out.println("3. Access your ledger");
-            System.out.println("4. Show how many movies");
-            System.out.println("5. Exit");
+            System.out.println("d. add a deposit?");
+            System.out.println("p. Make a Payment");
+            System.out.println("l. Access your ledger");
+            System.out.println("x. Exit");
             System.out.print("Pick here: ");
+            //User should input their data (scanner)
+            String choice = scanner.nextLine();
+
+            //switch - better but can use if statements
+
+
+            switch (choice) {
+                case "d" -> System.out.println("add a deposit");
+                case "p" -> System.out.println("make a payment");
+                case "l" -> System.out.println("access your ledger");
+                case "x" -> System.out.println("exit");
+
+                default -> System.out.println("Invalid choice, try again!");
+            }
+
         }
 
     }
-
 }
---
+++ src/main/java/com/yearup/Transaction.java
@@ -0,0 +1,69 @@
+package com.yearup;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    // section; property variables
+    //date|time|description|vendor|amount
+    LocalDate date;
+    LocalTime time;
+    String description;
+    String vendor;
+    float amount;
+
+
+    //constructor:
+    public Transaction (LocalDate date, LocalTime time, String description, String vendor, float amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+
+    }
+
+    //getters and setters
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    //Methods:
+}
